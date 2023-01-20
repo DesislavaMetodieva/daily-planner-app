696124659ca00813728c9ca6717c705808dccc55
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-01-20 17:47:11 +0000

    Added current date & time

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 
 // Pseudo code
 
-// 1. Create html element will hold all slot hours
+// 1. Create html element will hold all slot hours - done
+
 // 2. Add css classes for the element so it has blocks/lines for every hour slot from 9.00 until 17.00h ( 8 divs/blocks)
+
 // 3. Take the current time in format Friday, January 20nd and place it within element id="currentDay". format ("dddd MMMM Qo")
+
+var todayDate = moment();
+$('#currentDay').text(todayDate.format('dddd, MMMM Do'));
+
 // In order to color code each time block based on past, present, and future when the timeblock is viewed is to create a loop which is checking every block if it is in past hour, present hour or future hours. If equals to one of those 3 types of hours (past, present, future) I must add the relevant class to change the color of the corresponding block.
 // Create event listener for every event being added to the blocks
 // Store the event in Local storage so when the page reloads, the events are still rendering on the page in the same location
